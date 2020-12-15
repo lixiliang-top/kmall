b39c99c60ea7eab1560ccbea4fa7ebd0223a1ab2
lixiliang_glb <lixiliang_glb@kgc.com>
2020-12-15 17:21:20 +0800

    基本环境完成

--- kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -4,7 +4,8 @@ import com.kgc.kmall.bean.Member;
 import com.kgc.kmall.bean.MemberExample;
 import com.kgc.kmall.service.MemberService;
 import com.kgc.kmall.user.mapper.MemberMapper;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  * @author 李锡良
  * @create 2020-12-15 16:17
  */
+@Component
 @Service
 public class MemberServiceImpl implements MemberService {
     @Resource
